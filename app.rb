8fe09ddfd74c94e0dc47247470e9c092b4598f47
milligramme <milligramme.cc@gmail.com>
2012-08-01 14:40:59 +0900

    add source texts

--- app.rb
+++ app.rb
@@ -1,19 +1,32 @@
 # -*- coding: utf-8 -*-
 
-class Tw
+class Tatami
   def tl2points
     ar = []
+    txt = []
+    ret = {}
+
     Twitter.public_timeline.each do |d|
+      txt << d.text
       d.text.split(//).each do |s|
         ar << format("%x", s.unpack("U*")[0]).scan(/../)
       end
       ar.flatten!
     end
-    ar.join.scan(/..../).map{ |x| x.scan(/../).map{ |y| (y.hex) * 2}.join(',')}.join(' ')
+    ret['points'] = ar.join.scan(/..../).map{ |x| x.scan(/../).map{ |y| (y.hex) * 2}.join(',')}.join(' ')
+    ret['texts'] = txt.join
+    
+    ret
   end
 end
 
+get '/styles.css' do
+  sass :styles
+end
+
 get '/' do
-  @ar = Tw.new.tl2points
+  tl_src = Tatami.new.tl2points
+  @pts = tl_src['points']
+  @txt = tl_src['texts']
   haml :index
 end
